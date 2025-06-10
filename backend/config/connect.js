239ed6907e355e1f26952cc164eed98460c4103f
Praveena-manis <143096344+Praveena-manis@users.noreply.github.com>
2025-06-10 08:06:28 -0500

    Delete backend directory

--- backend/config/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("COnnected"))
-.catch((error)=>console.log("Error in Connection:"+error))
